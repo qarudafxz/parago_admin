74525e309680ba7c4fbf18430c79c2805b344606
qaruda <70809588+francistinao@users.noreply.github.com>
2023-05-25 00:00:08 +0800

    cleaning pre-loader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import logo from "../assets/parago_blue.svg";
-import parago from "../assets/parago_word.svg";
 
 function Preloader() {
 	return (
-		<div className='relative top-64'>
-			<img
-				src={logo}
-				alt='logo'
-				className='w-2/12 fade-in m-auto'
-			/>
-			<img
-				src={parago}
-				alt='parago'
-				className='w-2/12 m-auto fade-delay'
-			/>
-		</div>
+		<img
+			src={logo}
+			alt='logo'
+			className='w-2/12 fade-in relative m-auto top-72'
+		/>
 	);
 }
 
