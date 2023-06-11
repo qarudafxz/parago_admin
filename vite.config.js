13088b48509f7857e95ac19c3c1ba4427b33aba5
qaruda <70809588+francistinao@users.noreply.github.com>
2023-06-11 13:09:39 +0800

    integrated million.js for hyper optimized react app

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import million from "million/compiler";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
@@ -5,7 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [million.vite({ mode: "preact" }), react(), svgr()],
 	resolve: {
 		alias: {
 			"react-router-dom": path.resolve("./node_modules/react-router-dom"),
