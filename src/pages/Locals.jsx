bf8da4f108bf272a39c23d2b3819765395ae7ccf
qaruda <70809588+francistinao@users.noreply.github.com>
2023-06-11 23:21:58 +0800

    added animation to hover cards

--- src/pages/Locals.jsx
+++ src/pages/Locals.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import { MdEmojiTransportation } from "react-icons/md";
 import { IoMan } from "react-icons/io5";
@@ -34,7 +35,7 @@ function Locals() {
 					Dashboard/<span className='text-white font-semibold'>Locals</span>
 				</h1>
 				<h1 className='text-4xl text-white font-bold'>
-					View the local accomodations in your municipality!
+					Utilize the local accomodations in your municipality!
 				</h1>
 				<p className='text-white font-thin w-7/12 mt-4'>
 					View your local accomodations here. You can add details about the places'
@@ -43,18 +44,23 @@ function Locals() {
 			</div>
 			<div className='px-32 pt-14 grid grid-cols-3 gap-6'>
 				{locals.map((local, index) => (
-					<Link
-						to={local.link}
-						className='w-10/12 h-full shadow-md'
+					<motion.div
+						whileHover={{ scale: 1.042 }}
+						whileTap={{ scale: 0.9 }}
+						className='shadow-lg	rounded-md '
 						key={index}>
-						<div className='flex flex-col gap-2'>
-							<h1 className='flex gap-4 items-center font-bold text-2xl text-white bg-primary rounded-t-md py-4 px-8'>
-								{local.icon}
-								{local.title}
-							</h1>
-							<p className='p-4 rounded-b-md'>{local.description}</p>
-						</div>
-					</Link>
+						<Link
+							to={local.link}
+							className='w-10/12 h-full shadow-md'>
+							<div className='flex flex-col gap-2'>
+								<h1 className='flex gap-4 items-center font-bold text-2xl text-white bg-primary rounded-t-md py-4 px-8'>
+									{local.icon}
+									{local.title}
+								</h1>
+								<p className='p-4 rounded-b-md'>{local.description}</p>
+							</div>
+						</Link>
+					</motion.div>
 				))}
 			</div>
 		</div>
