2069b12aa0b0208eaada3133cac7ac6248173882
qaruda <70809588+francistinao@users.noreply.github.com>
2023-07-01 15:18:54 +0800

    added small ui for the transportation

--- src/pages/locals/Transportation.jsx
+++ src/pages/locals/Transportation.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { motion } from "framer-motion";
+
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
 function Transportation() {
 	return (
 		<>
 			<div className='w-full'>
-				<div className="w-full h-72 p-24 flex flex-col gap-2 shadow-2xl inset-0 bg-[url('https://i0.wp.com/touristspotsfinder.com/wp-content/uploads/2018/04/Top-10-Tourist-Spots-in-Agusan-del-Sur.jpg?fit=1200%2C629&ssl=1')]">
-					<div className='backdrop-blur-sm p-4 w-8/12'>
+				<div className="w-full h-72 p-10 flex flex-col gap-2 shadow-2xl inset-0 bg-[url('https://sataxi.co.za/wp-content/uploads/2022/09/SA-Taxi-Role-Of-Transportation.jpg')] bg-no-repeat">
+					<div className='p-4 w-8/12 backdrop-blur-sm'>
 						<h1 className='text-primary'>
 							Dashboard/
 							<span className='text-white font-semibold'>Transportations</span>
@@ -16,11 +20,28 @@ function Transportation() {
 							places
 						</h1>
 						<p className='text-white font-thin w-7/12 mt-4'>
-							These are the hidden gems you wish for the tourists to explore. You can
-							add details about the place by clicking edit
+							You can add details about the person responsible for the transportation
+							with their personal information
 						</p>
 					</div>
 					<div></div>
+				</div>
+				<div className='mt-10 px-14'>
+					<div className='flex gap-4 items-center'>
+						<motion.div
+							whileHover={{
+								initial: { x: 0 },
+								x: -9.5,
+							}}>
+							<Link to='/locals'>
+								<AiOutlineArrowLeft
+									size={30}
+									className='text-black'
+								/>
+							</Link>
+						</motion.div>
+						<h1>Locals</h1>
+					</div>
 				</div>
 			</div>
 		</>
